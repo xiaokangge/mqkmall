d4e052012de4bf150aa2d106a8e16df251d05f0d
xiaokangge <1203534085@qq.com>
2019-12-08 01:06:49 +0800

    mqkmall-api 新增bean以及service接口                                                           mqkmall-manage-service 新增 service实现类以及mapper接口                                                           mqkmall-manage-web新增catalogcontroller                                                           mqkmall-manage-service 端口9500                                                           mqkmall-mqnage-web 端口9600                                                           孟庆康 20191201 20:51:10                                                           新增 AttrController、PmsBaeAttrInfoService、、、、、、、、、                                                           新增spu实体类，controller，service，servieimpl  孟庆康 20191203 00：16                                                           fastdfs客户端整合，引入fastdfs-client-java项目，并将其打成pom依赖，放到mqkmall-manage-web的pom.xml文件中 孟庆康 20191208 01:06:11

--- mqkmall-manage-web/src/test/java/com/mqkmall/manage/MqkmallManageWebApplicationTests.java
+++ mqkmall-manage-web/src/test/java/com/mqkmall/manage/MqkmallManageWebApplicationTests.java
@@ -1,14 +1,28 @@
 package com.mqkmall.manage;
 
 
+import org.csource.common.MyException;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class MqkmallManageWebApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException, MyException {
+
+            String tracker = MqkmallManageWebApplicationTests.class.getResource("/tracker.conf").getPath();
+        ClientGlobal.init(tracker);
+        TrackerClient trackerClient = new TrackerClient();
+        TrackerServer trackerServer = trackerClient.getConnection();
+        StorageClient storageClient = new StorageClient(trackerServer,null);
+
     }
 
 }
